a04c48ea1c16569ca2cb50f8a8d6707e4f223cf5
Katerina S <89135150+KaterinaAU@users.noreply.github.com>
2022-03-07 22:11:25 +1100

    Add files via upload

---
+++ contacts.js
@@ -0,0 +1,24 @@
+const contacts = [
+  {
+    name: "Beyonce",
+    imgURL:
+      "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
+    phone: "+123 456 789",
+    email: "b@beyonce.com"
+  },
+  {
+    name: "Paulo Coelho",
+    imgURL:
+      "https://assets.penguinrandomhouse.com/wp-content/uploads/2020/09/25140235/2400x2400-Paulo-Coelho.png",
+    phone: "+987 654 321",
+    email: "coelho@nowhere.com"
+  },
+  {
+    name: "Leonardo Dicaprio",
+    imgURL:
+      "http://d17zbv0kd7tyek.cloudfront.net/wp-content/uploads/2015/06/leonardo-dicaprio-fb.jpg",
+    phone: "+918 372 574",
+    email: "gmail@dicaprio.com"
+  }
+];
+export default contacts;
